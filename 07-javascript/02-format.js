5f120dbac2a740750321eef8d66c7f72f120758e
Olivia Decker <oliviajanebruni@gmail.com>
2024-11-19 00:03:47 -0800

    Completed exercises 01-05.

--- 07-javascript/02-format.js
+++ 07-javascript/02-format.js
@@ -4,7 +4,11 @@ const formatItems = (input) => {
   // for reference, see the MDN Number page:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
 
-  return input;
+  for (let x in input) {
+    input[x] = " " + input[x].toFixed(3);
+  }
+
+  return input.toString();
 };
 
 console.log(formatItems([1, 4, 17, 26, 41]));
--- 07-javascript/04-dom.js
+++ 07-javascript/04-dom.js
@@ -1,5 +1,10 @@
 // Exercise #1:
 // when the user clicks the 'copy' button, copy the user input to the output area
+document.getElementById("copy").addEventListener("click", copyInput);
+function copyInput() {
+  const inputVal = document.getElementById("userInput1").value;
+  document.querySelector("div.output").textContent = inputVal;
+}
 
 // fetch JavaScript objects representing specific elements in the DOM
 
@@ -11,7 +16,15 @@
 // when the user enters input text, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
+const textBox = document.getElementById("userInput2");
+const outText = document.createElement("p");
+const container = document.getElementById("inputEventExample");
 
 // add an event listener on the target element
+textBox.addEventListener("change", outputText);
 
 // callback function to handle event
+function outputText() {
+  outText.textContent = textBox.value;
+  container.appendChild(outText);
+}
