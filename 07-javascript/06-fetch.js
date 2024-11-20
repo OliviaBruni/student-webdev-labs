5e21e802173a8ea393779c530dba04020d685652
Olivia Decker <oliviajanebruni@gmail.com>
2024-11-19 20:16:14 -0800

    Completed Lab 07-javascript.

--- 07-javascript/06-fetch.js
+++ 07-javascript/06-fetch.js
@@ -2,9 +2,49 @@ const url = "https://anapioficeandfire.com/api/books/";
 
 const app = document.querySelector("#books");
 
+const addBookToDOM = (book) => {
+  let container = document.createElement("div");
+  let title = document.createElement("h2");
+  let author = document.createElement("p");
+  let release = document.createElement("p");
+  let pageCount = document.createElement("p");
+
+  title.textContent = book.name;
+  author.textContent = "by " + book.authors[0];
+  release.textContent = book.released.slice(0, 4);
+  pageCount.textContent = book.numberOfPages + " pages";
+
+  container.append(title);
+  container.append(author);
+  container.append(release);
+  container.append(pageCount);
+
+  app.append(container);
+};
+
 const fetchData = (url) => {
   // Fetch all books from the API of Ice and Fire and append them to the DOM
-  fetch(url).then((Response) => Response.json);
   // Create an element for each book that contains title, author, publication year, and number of pages
   // Update the styles in JavaScript to center all the books in the container given
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((book) => {
+        addBookToDOM(book);
+      });
+      let loader = document.getElementById("loading");
+      app.removeChild(loader);
+    })
+    .catch((error) => {
+      console.error(error);
+
+      let element = document.createElement("div");
+      element.textContent = "An error occured. Please reload the page.";
+      app.append(element);
+
+      let loader = document.getElementById("loading");
+      app.removeChild(loader);
+    });
 };
+
+fetchData(url);
--- 07-javascript/reflection.md
+++ 07-javascript/reflection.md
@@ -4,6 +4,14 @@
 
 Enter your reflections on this section here.
 
+This lab in particular required a lot of research in order for me to complete. I understand that with JavaScript being introduced, this was pretty much a given, but I spent a lot of time and effort figuring out how to complete this lab. I was able to do sections 01-05 on my own, however sections 06-07 I needed to follow the video walkthrough. However, I learned a ton about JavaScript and have these lab exercises to thank.
+
 ### Accessibility
 
 Enter your reflections on this section here.
+
+1. Wave AIM - no issues, nothing to fix
+2. w3c validation - issues with HTML script tag, as well as 06-fetch.html loading img styling.
+3. Prettier was automatically implemented, no issues.
+
+   Generally, the implementation of accessibility elements were extremely easy in this lab. Especially given that the HTML and other elements were made to be accessible from the start.
